138280546bd339fbb98d810ecd5ad980ad490a9f
Purple_Creativity <thepurplecreativity@gmail.com>
2024-12-13 17:46:02 +0100

    jsdoc the main class

--- src/main.ts
+++ src/main.ts
@@ -16,29 +16,67 @@ import fetchUsersByNames, { type fetchUsersByNamesTypes } from "./functions/lega
 
 export type * from "./types/fetchHandler.js";
 export type * from "./types/shared.js";
+/**
+ * Main class for interacting with the Roblox API through various endpoints.
+ */
 export default class BloxFetch {
+    /**
+     * Instance of the internal fetch handler responsible for making API requests.
+     */
     readonly fetchHandler: FetchHandler;
 
     //? UsersV1
-    readonly fetchUsernameHistory: fetchUsernameHistoryType;
-    readonly fetchBirthdate: fetchBirthdateType;
-    readonly fetchUserById: fetchUserByIdType;
-    readonly fetchUsersByIds: fetchUsersByIdsType;
-    readonly fetchUsersByNames: fetchUsersByNamesTypes;
-    //? PresenceV1
-    readonly fetchUsersPresence: fetchUsersPresenceType;
-    //? GroupsV1
 
+    /**
+     * Fetches a user's username history by their Id.
+     */
+    readonly fetchUsernameHistory: fetchUsernameHistoryType;
+
+    /**
+     * Fetches a user's birthdate information.
+     */
+    readonly fetchBirthdate: fetchBirthdateType;
+
+    /**
+     * Fetches detailed information about a user by their Id.
+     */
+    readonly fetchUserById: fetchUserByIdType;
+
+    /**
+     * Fetches information about multiple users by their Ids.
+     */
+    readonly fetchUsersByIds: fetchUsersByIdsType;
+
+    /**
+     * Fetches information about multiple users by their usernames.
+     */
+    readonly fetchUsersByNames: fetchUsersByNamesTypes;
+
+    //? PresenceV1
+
+    /**
+     * Fetches the online presence of multiple users.
+     */
+    readonly fetchUsersPresence: fetchUsersPresenceType;
+
+    //? GroupsV1 (Future implementation placeholder)
+
+    /**
+     * Initializes a new instance of the BloxFetch class.
+     */
     constructor() {
         this.fetchHandler = new FetchHandler({});
+
         //? UsersV1
         this.fetchUsernameHistory = fetchUsernameHistory;
         this.fetchBirthdate = fetchBirthdate;
         this.fetchUserById = fetchUserById;
         this.fetchUsersByIds = fetchUsersByIds;
         this.fetchUsersByNames = fetchUsersByNames;
+
         //? PresenceV1
         this.fetchUsersPresence = fetchUsersPresence;
-        //? GroupsV1
+
+        //? GroupsV1 (Future implementation placeholder)
     }
 }
